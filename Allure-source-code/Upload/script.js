d35f01afe267dac620927a285d0991663101532f
CeriCam <cherycamberly2005@gmail.com>
2025-01-07 20:00:54 +0700

    Add files via upload

---
+++ Allure-source-code/Upload/script.js
@@ -0,0 +1,19 @@
+function goToHome(){
+    window.location.href = "../index.html";
+}
+
+function goToGallery(){
+    window.location.href = "../Gallery/galleryPage.html";
+}
+
+function goToCalendar(){
+    window.location.href = "../Calendar/calendarPage.html";
+}
+
+function goToProfile(){
+    window.location.href = "../Profile/profilePage.html";
+}
+
+function goToUpload(){
+    window.location.href = "uploadPage.html";
+}
